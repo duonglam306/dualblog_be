489a5162492e20aea1b0cff184c68db407cb55d3
duonglt3006 <duong.lamthanh3006@hcmut.edu.vn>
2023-03-30 20:17:49 +0700

    Fix update article

--- controllers/mailerControllers.js
+++ controllers/mailerControllers.js
@@ -7,52 +7,51 @@ import hbs from "nodemailer-express-handlebars";
 // @route   GET /api/mailer/signUp
 // @access  Public
 const sendMailSignUp = asyncHandler(async (req, res) => {
-    const { username, email } = req.body;
-    let transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+  const { username, email } = req.body;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const handlebarOptions = {
-        viewEngine: {
-            extName: ".handlebars",
-            partialsDir: path.resolve("./controllers/signUp"),
-            defaultLayout: false,
-        },
-        viewPath: path.resolve("./controllers/signUp"),
-        extName: ".handlebars",
-    };
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir: path.resolve("./controllers/signUp"),
+      defaultLayout: false,
+    },
+    viewPath: path.resolve("./controllers/signUp"),
+    extName: ".handlebars",
+  };
 
-    transporter.use("compile", hbs(handlebarOptions));
+  transporter.use("compile", hbs(handlebarOptions));
 
-    var mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: "Get started with DualBlog",
-        template: "email",
-        context: {
-            username: username,
-            url_client: process.env.CLIENT_URL,
-        },
-    };
+  var mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "Get started with DualBlog",
+    template: "email",
+    context: {
+      username: username,
+      url_client: process.env.CLIENT_URL,
+    },
+  };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.status(401);
-            throw new Error("Send verify account email failed");
-        } else {
-            res.json({
-                message: info.response,
-            });
-        }
-    });
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Send verify account email failed");
+    } else {
+      res.json({
+        message: info.response,
+      });
+    }
+  });
 });
-
 
 export { sendMailSignUp };
